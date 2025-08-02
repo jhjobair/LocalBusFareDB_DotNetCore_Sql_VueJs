4efcd5bbfce01664d8b614161c8d9cb0b98f1f1f
rhrashal <rhrashal@gmail.com>
2025-08-03 00:22:50 +0600

    initial upload

---
+++ WebAPI/Helpers/DataContext.cs
@@ -0,0 +1,29 @@
+namespace WebApi.Helpers;
+
+using Microsoft.EntityFrameworkCore;
+using WebApi.Entities;
+
+//public class DataContext : DbContext
+//{
+//    protected readonly IConfiguration Configuration;
+
+//    public DataContext(IConfiguration configuration)
+//    {
+//        Configuration = configuration;
+//    }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder options)
+//    {
+//        // in memory database used for simplicity.
+//        options.UseInMemoryDatabase("UserDb");
+//    }
+
+//    public DbSet<User> Users { get; set; }
+//}
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    public DbSet<User> Users { get; set; } // example entity
+}
---
+++ WebAPI/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using System.Text.Json.Serialization;
+using WebApi.Helpers;
+using WebApi.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+{
+    var services = builder.Services;
+    var configuration = builder.Configuration;
+
+    // SQL Server connection
+    services.AddDbContext<DataContext>(options =>
+        options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+    // CORS
+    services.AddCors();
+
+    // Controllers with JSON options
+    services.AddControllers().AddJsonOptions(x =>
+    {
+        x.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+        x.JsonSerializerOptions.DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull;
+    });
+
+    // AutoMapper
+    services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+    // Swagger/OpenAPI
+    services.AddEndpointsApiExplorer();
+    services.AddSwaggerGen(c =>
+    {
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "My Web API",
+            Version = "v1",
+            Description = "API documentation for My Web API"
+        });
+    });
+
+    // Application services
+    services.AddScoped<IUserService, UserService>();
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+{
+    app.UseCors(x => x
+        .AllowAnyOrigin()
+        .AllowAnyMethod()
+        .AllowAnyHeader());
+
+    // Error handling middleware
+    app.UseMiddleware<ErrorHandlerMiddleware>();
+
+    // Swagger UI
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Web API v1");
+        c.RoutePrefix = string.Empty; // Swagger UI at root URL
+    });
+
+    app.MapControllers();
+}
+
+// Run the app
+app.Run("http://localhost:9080");
+
+
+//dotnet ef migrations add InitialCreate
+//dotnet ef database update
